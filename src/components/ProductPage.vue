0c621a328d04033cf51f564df8a668e20d2d8458
ajay bodara <ad.bodara@gmail.com>
2021-06-16 22:37:58 +0530

    remove un-used code

--- src/components/ProductPage.vue
+++ src/components/ProductPage.vue
@@ -49,17 +49,16 @@ export default {
 	var docRef = db.collection('products').doc(this.$route.params.id);
 	docRef.get().then((doc)=> {
 	    if (doc.exists) {
-	        console.log("Document data:", doc.data().product_id);
 	        this.productId = doc.data().product_id
 	        this.productName = doc.data().product_name
 	        this.productPrice = doc.data().product_price
 	        this.loaded = true
 	    } else {
-	        // doc.data() will be undefined in this case
 	        console.log("No such document!");
 	    }
 	}).catch(function(error) {
-	    console.log("Error getting document:", error);
+			console.log("Error getting document:", error);
+			console.log("profuct error:", error);
 	});
 
   }
